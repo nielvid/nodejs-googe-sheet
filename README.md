e79f7b0c283e129b4097e2ae2c008fdac9765b1d
nielvid <nielvidservices@gmail.com>
2024-04-27 12:54:46 +0100

    script to post to and read from google sheet

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to Use Google Sheets API with Nodejs
+
+## Post and Read from google sheet directly from nodejs
+
+- Create a spreadsheet on Google sheets
+- Enable Google Sheets API in our project on Google developers console
+- Create a service account
+- Share the spreadsheet created in step 1 with the service account created in step1
---
+++ index.js
@@ -0,0 +1,99 @@
+const { google } = require('googleapis')
+const dotenv = require('dotenv')
+dotenv.config()
+
+async function authenticate() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,  // add this to system variables,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  })
+  const authClient = await auth.getClient()
+  const sheet =  google.sheets({
+    version: 'v4',
+    auth: authClient,
+  })
+return {user: authClient, sheet}
+}
+
+
+const spreadsheetId = ''
+const sheetName = 'Sheet1'
+
+
+
+   
+
+async function getSpreadSheetInfo(spreadsheetId) {
+  const { sheet } = await authenticate()
+  const info = sheet.spreadsheets.get({
+    spreadsheetId,
+  })
+  return info
+}
+
+
+async function readFromSheet({ spreadsheetId, sheetName}) {
+  const { sheet } = await authenticate()
+
+  const data = sheet.spreadsheets.values.get(
+    {
+      spreadsheetId,
+      range: sheetName,
+    }).then((res) => {
+     return res.data.values
+    }).catch((err) => {
+      console.log(err, 'error reading from sheet')
+    })
+     
+    return data
+}
+
+async function postToSheet({ spreadsheetId, sheetName, data }) {
+  const { sheet } = await authenticate()
+  const response = sheet.spreadsheets.values.append(
+    {
+      spreadsheetId,
+      range: sheetName,
+      valueInputOption: 'USER_ENTERED',
+      //   insertDataOption: 'INSERT_ROWS',
+      resource: {
+        values: data,
+      },
+    }).then((res) => {
+     return res
+    }).catch((err) => {
+      console.log(err, 'error posting to sheet')
+    })
+    return response
+}
+  const data = [
+    ['johnson', 'johnson@gmail.com'], // Row 1
+    [' paulsco', 'paulsco@yahoo.com'], // Row 2
+  ]
+
+
+//  readFromSheet({ spreadsheetId, sheetName })
+//    .then((res) => {
+//      console.log(res, 'data from sheet')
+//    })
+//    .catch((err) => {
+//      console.log(err)
+//    })
+
+// postToSheet({ spreadsheetId, sheetName, data })
+//   .then((res) => {
+//     console.log(res.status, 'sucessfully created record')
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+// getSpreadSheetInfo(spreadsheetId)
+//   .then((res) => {
+//     console.log(res, 'sheet info')
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+
